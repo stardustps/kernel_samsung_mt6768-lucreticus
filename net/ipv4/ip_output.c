30486e4937c8179f6f84826a63c63ba46dae948f
SlamZDank <mohammedaminslama@gmail.com>
2026-09-19 23:12:15 +0100

    fix(ipv4): initialize gso_size before use

--- net/ipv4/ip_output.c
+++ net/ipv4/ip_output.c
@@ -1519,6 +1519,7 @@ struct sk_buff *ip_make_skb(struct sock *sk,
 	cork->flags = 0;
 	cork->addr = 0;
 	cork->opt = NULL;
+  cork->base.gso_size = 0;
 	err = ip_setup_cork(sk, cork, ipc, rtp);
 	if (err)
 		return ERR_PTR(err);
