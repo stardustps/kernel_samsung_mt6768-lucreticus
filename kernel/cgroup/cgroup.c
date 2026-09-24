d1e2563c58b0835cd9ac1044874c37c5693447e5
dotarma <ncatt@tuta.io>
2026-09-24 16:54:11 +0700

    feat: integrate DroidSpaces non-GKI support
    Apply the official cgroup prefix compatibility behavior for the backported cgroup implementation, enforce the available mandatory legacy-kernel options, and build bridge netfilter into the packaged kernel.

--- kernel/cgroup/cgroup.c
+++ kernel/cgroup/cgroup.c
@@ -1583,6 +1583,14 @@ static void cgroup_rm_file(struct cgroup *cgrp, const struct cftype *cft)
 		spin_unlock_irq(&cgroup_file_kn_lock);
 	}
 
+	if (cft->ss && (cgrp->root->flags & CGRP_ROOT_NOPREFIX) &&
+	    !(cft->flags & CFTYPE_NO_PREFIX)) {
+		snprintf(name, CGROUP_FILE_NAME_MAX, "%s.%s",
+			 cgroup_on_dfl(cgrp) ? cft->ss->name :
+			 cft->ss->legacy_name, cft->name);
+		kernfs_remove_by_name(cgrp->kn, name);
+	}
+
 	kernfs_remove_by_name(cgrp->kn, cgroup_file_name(cgrp, cft, name));
 }
 
@@ -3780,6 +3788,15 @@ static int cgroup_add_file(struct cgroup_subsys_state *css, struct cgroup *cgrp,
 		spin_lock_irq(&cgroup_file_kn_lock);
 		cfile->kn = kn;
 		spin_unlock_irq(&cgroup_file_kn_lock);
+	}
+
+	/* Keep the prefixed alias expected by container runtimes. */
+	if (cft->ss && (cgrp->root->flags & CGRP_ROOT_NOPREFIX) &&
+	    !(cft->flags & CFTYPE_NO_PREFIX)) {
+		snprintf(name, CGROUP_FILE_NAME_MAX, "%s.%s",
+			 cgroup_on_dfl(cgrp) ? cft->ss->name :
+			 cft->ss->legacy_name, cft->name);
+		kernfs_create_link(cgrp->kn, name, kn);
 	}
 
 	return 0;
